f7b4a6d6f4d7484b965ebe216aabe90651d5d8b1
muriloFsaidel <murilofernandessaidel@hotmail.com>
2022-05-17 19:10:39 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,26 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
 import NavBar from "components/NavBar";
 
 
 //componente react (a função que retorna um conjunto de tags html)
 function App() {
   return (
-    <NavBar /> 
+    <BrowserRouter>
+      <NavBar />
+        <Routes>
+          <Route path="/" element={<Listing />}/>
+          <Route path="/form">
+            <Route path=':movieId' element={<Form />}/>
+          </Route>
+        </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
- 
